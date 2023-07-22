f294eaca1eb8d60c68823fa5dc9d36105d9f4b12
Horev Vladimir <vshorev@yandex.ru>
2023-07-22 18:40:01 +0300

    вроде заработала фильтрация

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-
+//импот стилей
 import './App.css';
+//импорт картинок с папки images
 import image1 from './images/image1.png';
 import image2 from './images/image2.png';
 import image3 from './images/image3.png';
 import arrow from './images/arrow.svg';
+// импорт хука
 import React, { useState } from "react"
 
 
-
+// сам компонет
 function App() {
+
+  // создаем массив который будем рендерить в верстку
   const posts = [
     {
       id: 1,
@@ -27,32 +31,44 @@ function App() {
     },
   ];
 
-  const [show, setShow] = useState(true);
-  const [value, setValue] = useState();
-  const [values, setValues] = useState(posts);
-  // const [valuess, setValuess] = useState(posts);
+  // создаем наши состояния
 
+  // состояние для обображения выпадющего меню
+  const [show, setShow] = useState(true);
+  //состояние для подстановки в инпут выбранного здачения
+  const [value, setValue] = useState();
+  // ссотояние для фильтрации
+  const [values, setValues] = useState(posts);
+
+// функция для скрыть/ открыть выпадающее меню
   function press(auto) {
     setValue(auto)
     setShow(!show)
   }
-
+ 
+  // обрабочик фильтрации
   const handleChange = (event) => {
+    // складываем значение  напеченные в инпуте
     let result = event.target.value;
+    //создаем копию массива
+    let copyPosts = posts
+    // если мы набрали один из имен которые надо отфильтровать тогла фильтруем массив и обновляем состояние values
     if (result === 'skoda' || result === 'bmv' || result === 'audi') {
       let filteredValues = posts.filter(element => element.auto == result)
       setValues(filteredValues)
     }
-    
-   
+    // если стираем в интупе тогла подставляем то что осталось в event.target.value
     else {
       setValue(result)
+      // если все стерли, тогда вставляем скопированный массив, чтобы по нвой выбрать
+      if (result == '') {
+        setValues(copyPosts)
+      }
     }
-
   }
 
 
-
+// мой компонент в котором происходит рендеринг с массива с помощью метода .map
   const sidebar = (
     <div className='size2'>
       <ul className={show ? "show" : ""}>
@@ -67,11 +83,12 @@ function App() {
       </ul>
     </div>
   );
-//  value={value || ''} иногда не работаест пустым значением а так убирает ворнинг
+  //  value={value || ''} нету больше ворнинга
+
+  //возвращаемая верстка
   return (
     <div>
       <div className='position' onClick={() => setShow(!show)}>
-        
         <input type='text' className='positioninput' value={value || ''} onChange={handleChange}></input>
         <div> <img src={arrow} className='arrow' alt='none'></img> </div>
       </div>
