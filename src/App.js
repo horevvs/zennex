73d3ed65ca2e9d9ebb596f494671e40b1167bdb4
Horev Vladimir <vshorev@yandex.ru>
2023-07-24 17:54:28 +0300

    фильтрация реализована

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ import arrow from './images/arrow.svg';
 import React, { useState } from "react"
 
 
-// сам компонет
 function App() {
 
-  // создаем массив который будем рендерить в верстку
+
+
   const posts = [
     {
       id: 1,
@@ -29,72 +29,160 @@ function App() {
       auto: 'audi',
       img: image3,
     },
+    {
+      id: 4,
+      auto: 'aurus',
+      img: image3,
+    }
   ];
 
-  // создаем наши состояния
-
-  // состояние для обображения выпадющего меню
-  const [show, setShow] = useState(true);
-  //состояние для подстановки в инпут выбранного здачения
-  const [value, setValue] = useState();
-  // ссотояние для фильтрации
   const [values, setValues] = useState(posts);
+  const [value, setValue] = useState();
 
-// функция для скрыть/ открыть выпадающее меню
-  function press(auto) {
-    setValue(auto)
-    setShow(!show)
-  }
- 
-  // обрабочик фильтрации
   const handleChange = (event) => {
+
+    let input, filter, ul, li, a, i, txtValue;
+
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    console.log(filter)
     // складываем значение  напеченные в инпуте
-    let result = event.target.value;
-    //создаем копию массива
-    let copyPosts = posts
-    // если мы набрали один из имен которые надо отфильтровать тогла фильтруем массив и обновляем состояние values
-    if (result === 'skoda' || result === 'bmv' || result === 'audi') {
-      let filteredValues = posts.filter(element => element.auto == result)
-      setValues(filteredValues)
-    }
-    // если стираем в интупе тогла подставляем то что осталось в event.target.value
-    else {
-      setValue(result)
-      // если все стерли, тогда вставляем скопированный массив, чтобы по нвой выбрать
-      if (result == '') {
-        setValues(copyPosts)
+    let result = event.target.value.toUpperCase();
+
+    ul = document.getElementById("myUL");
+    li = document.getElementsByTagName('li')
+
+    for (i = 0; i < li.length; i++) {
+      a = li[i].textContent
+      // txtValue = a.toUpperCase().indexOf(result)
+
+
+
+      if (a.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
       }
+
+
     }
+
+    setValue(result)
   }
 
 
-// мой компонент в котором происходит рендеринг с массива с помощью метода .map
-  const sidebar = (
-    <div className='size2'>
-      <ul className={show ? "show" : ""}>
-        {values.map((post) =>
-          <div key={post.id}>
-            <div className='flexb point' onClick={() => press(post.auto)}  >
-              <div> {post.auto}</div>
-              <div> <img className='size' src={post.img} alt='нету' /> </div>
-            </div>
-          </div>
-        )}
-      </ul>
-    </div>
-  );
-  //  value={value || ''} нету больше ворнинга
 
-  //возвращаемая верстка
   return (
     <div>
-      <div className='position' onClick={() => setShow(!show)}>
-        <input type='text' className='positioninput' value={value || ''} onChange={handleChange}></input>
-        <div> <img src={arrow} className='arrow' alt='none'></img> </div>
+      <div  >
+
+
+
+        <input type='text' id='myInput' value={value || ''} onChange={handleChange} ></input>
+        <div >
+          <ul className='myUL'>
+            {values.map((post) =>
+              <li key={post.id}>
+                {post.auto}
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
-      {sidebar}
     </div>
   );
+
+
+
 }
+
+
+// // сам компонет
+// function App() {
+
+//   // создаем массив который будем рендерить в верстку
+//   const posts = [
+//     {
+//       id: 1,
+//       auto: 'bmv',
+//       img: image1,
+//     },
+//     {
+//       id: 2,
+//       auto: 'skoda',
+//       img: image2,
+//     },
+//     {
+//       id: 3,
+//       auto: 'audi',
+//       img: image3,
+//     },
+//   ];
+
+//   // создаем наши состояния
+
+//   // состояние для обображения выпадющего меню
+//   const [show, setShow] = useState(true);
+//   //состояние для подстановки в инпут выбранного здачения
+//   const [value, setValue] = useState();
+//   // ссотояние для фильтрации
+//   const [values, setValues] = useState(posts);
+
+// // функция для скрыть/ открыть выпадающее меню
+//   function press(auto) {
+//     setValue(auto)
+//     setShow(!show)
+//   }
+
+//   // обрабочик фильтрации
+//   const handleChange = (event) => {
+//     // складываем значение  напеченные в инпуте
+//     let result = event.target.value;
+//     //создаем копию массива
+//     let copyPosts = posts
+//     // если мы набрали один из имен которые надо отфильтровать тогла фильтруем массив и обновляем состояние values
+//     if (result === 'skoda' || result === 'bmv' || result === 'audi') {
+//       let filteredValues = posts.filter(element => element.auto == result)
+//       setValues(filteredValues)
+//     }
+//     // если стираем в интупе тогла подставляем то что осталось в event.target.value
+//     else {
+//       setValue(result)
+//       // если все стерли, тогда вставляем скопированный массив, чтобы по нвой выбрать
+//       if (result == '') {
+//         setValues(copyPosts)
+//       }
+//     }
+//   }
+
+
+// // мой компонент в котором происходит рендеринг с массива с помощью метода .map
+//   const sidebar = (
+//     <div className='size2'>
+//       <ul className={show ? "show" : ""}>
+//         {values.map((post) =>
+//           <div key={post.id}>
+//             <div className='flexb point' onClick={() => press(post.auto)}  >
+//               <div> {post.auto}</div>
+//               <div> <img className='size' src={post.img} alt='нету' /> </div>
+//             </div>
+//           </div>
+//         )}
+//       </ul>
+//     </div>
+//   );
+//   //  value={value || ''} нету больше ворнинга
+
+//   //возвращаемая верстка
+//   return (
+//     <div>
+//       <div className='position' onClick={() => setShow(!show)}>
+//         <input type='text' className='positioninput' value={value || ''} onChange={handleChange}></input>
+//         <div> <img src={arrow} className='arrow' alt='none'></img> </div>
+//       </div>
+//       {sidebar}
+//     </div>
+//   );
+// }
 
 export default App;
