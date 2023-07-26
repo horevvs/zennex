ce32e7ce4d3cd7ee71bd25963bc5aaa0ca852dc8
Horev Vladimir <vshorev@yandex.ru>
2023-07-26 18:29:29 +0300

    add fonts

--- src/components/Select.js
+++ src/components/Select.js
@@ -6,6 +6,18 @@ import image3 from '../images/image3.png';
 import image4 from '../images/image4.png';
 import arrow from '../images/arrow.svg';
 import React, { useState } from "react"
+import styled from 'styled-components'
+
+const Teg = styled.div`
+color: #0091FF;
+font-family: 'Permanent Marker', cursive;
+`
+
+const Bord = styled.div`
+border: solid #0091FF 1px;
+margin-top: -25px;
+background-color: white;
+`
 
 
 function Select() {
@@ -28,7 +40,7 @@ function Select() {
     },
     {
       id: 4,
-      auto: 'AURUS',
+      auto: 'ROLLS-ROYCE',
       img: image4,
     }
   ];
@@ -43,9 +55,11 @@ function Select() {
   const handleChange = (event) => {
 
     let input, li, textcontents;
+
     input = document.getElementById("myInput").value.toUpperCase();
     let result = event.target.value.toUpperCase();
-    li = document.getElementsByTagName('li')
+    li = document.getElementsByTagName('li');
+
     for (let i = 0; i < li.length; i++) {
       textcontents = li[i].textContent
       if (textcontents.toUpperCase().indexOf(input) > -1) {
@@ -54,15 +68,21 @@ function Select() {
         li[i].style.display = "none";
       }
     }
-    setValue(result)
+
+    setValue(result);
+    if(event.target.value == ''){
+      setShow(!show);
+    }
+
   }
 
   // функция множественного выбора элементов
   function press(auto) {
-    if (deleteArray.indexOf(auto) === -1) {
+    if (deleteArray.indexOf(auto) === -1)
+     {
       deleteArray.push(auto)
       let resultPush = deleteArray.map((item) => `${item}`);
-      setValue(resultPush)
+      setValue(resultPush);
     }
     else {
       for (let i = 0; i <= deleteArray.length; i++) {
@@ -70,51 +90,50 @@ function Select() {
           const index = deleteArray.findIndex(item => item === deleteArray[i]);
           deleteArray.splice(index, 1);
           let resultDelete = deleteArray.map((item) => `${item}`);
-          setValue(resultDelete)
+          setValue(resultDelete);
         }
       }
     }
   }
 
   function ShowMenu() {
-    setShow(!show)
+    setShow(!show);
   }
 
   document.onclick = function (event) {
     if (!event.target.matches('.hide')) {
-      setShow(true)
+      setShow(true);
     }
   }
 
   return (
     <div>
-      <form  >
+      <form>
         <fieldset className='positiondiv hide'>
           <legend className='legend hide'>Мои автомобили</legend>
+
           <div className=' hide'>
-            <div className=' hide'>
-              <input onClick={ShowMenu} type='text' id='myInput' value={value || ''} onChange={handleChange} placeholder='Выберите авто..' className='inputsize point hide '  ></input>
-              <img onClick={ShowMenu} src={arrow} className='arrow point hide' alt='none'></img>
-            </div>
+              <div className=' hide'>
+                <input onClick={ShowMenu} type='text' id='myInput' value={value || ''} onChange={handleChange} placeholder='Выберите авто..' className='inputsize point hide '  ></input>
+                <img onClick={ShowMenu} src={arrow} className='arrow point hide' alt='none'></img>
+              </div>
 
-
-
-            <div className={show ? "show ulBackground hide" : "ulBackground hide"} >
-              <ul className='myUL hide'>
-                {values.map((post) =>
-                  <li key={post.id} onClick={() => press(post.auto)} >
-                    <div className='flexb point hide'>
-                      <div className='fonts hide'> {post.auto} </div>
-                      <div className='point hide'> <img className='size hide' src={post.img} alt='' /> </div>
-                    </div>
-                  </li>
-                )}
-              </ul>
-            </div>
+              <div className={show ? "show  hide" : " hide"} >
+                <Bord className='myUL hide'>
+                  {values.map((post) =>
+                    <li key={post.id} onClick={() => press(post.auto)} >
+                      <Teg className='flexb point hide'>
+                        <div className='fonts hide'> {post.auto} </div>
+                        <div className='point hide'> <img className='size hide' src={post.img} alt='' /> </div>
+                      </Teg>
+                    </li>
+                  )}
+                </Bord>
+              </div>
           </div>
         </fieldset>
       </form>
-    </div >
+    </div>
   );
 }
 export default Select;
