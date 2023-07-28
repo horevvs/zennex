78ef3d5278c161bef4cac871664a8386b827d1fd
Horev Vladimir <vshorev@yandex.ru>
2023-07-28 19:13:33 +0300

    вынес в импорты

---
+++ src/components/PostsAndstyleSettings.js
@@ -0,0 +1,35 @@
+
+import image1 from '../images/image1.png';
+import image2 from '../images/image2.png';
+import image3 from '../images/image3.png';
+import image4 from '../images/image4.png';
+
+const posts = [
+    {
+      id: 1,
+      auto: 'BMV',
+      img: image1,
+    },
+    {
+      id: 2,
+      auto: 'SKODA',
+      img: image2,
+    },
+    {
+      id: 3,
+      auto: 'AUDI',
+      img: image3,
+    },
+    {
+      id: 4,
+      auto: 'ROLLS-ROYCE',
+      img: image4,
+    }
+  ];
+  
+  
+  
+
+  
+  export default posts
+  
